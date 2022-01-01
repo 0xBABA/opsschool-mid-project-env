f21c61814765f135dd2c2fa1deb912ed033dc20d
yoad <yoadlanger@gmail.com>
2022-01-01 18:49:42 +0200

    added ALB to allow access to consul ui with ease

--- terraform/consul.tf
+++ terraform/consul.tf
@@ -98,3 +98,12 @@ resource "aws_security_group_rule" "consul_jenkins_server" {
   description              = "Allow ping"
   security_group_id        = aws_security_group.consul-sg.id
 }
+
+resource "aws_security_group_rule" "consul_alb" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.consul_alb_sg.id
+  security_group_id        = aws_security_group.consul-sg.id
+}
