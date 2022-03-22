7c918577c8f1eb0faa320f2179cbfb195ad32d80
yoad <yoadlanger@gmail.com>
2022-03-22 20:26:21 +0200

    cleanup

--- terraform/sg-elk.tf
+++ terraform/sg-elk.tf
@@ -40,22 +40,3 @@ resource "aws_security_group_rule" "kibana_tcp" {
   security_group_id = aws_security_group.elk_sg.id
 }
 
-#TODO: might not be required if using only filebeat
-# resource "aws_security_group_rule" "logstash_tcp" {
-#   type              = "ingress"
-#   from_port         = 5044
-#   to_port           = 5044
-#   protocol          = "tcp"
-#   cidr_blocks       = concat(var.private_subnet_cidrs, var.public_subnet_cidrs)
-#   description       = "Allow elk rest tcp"
-#   security_group_id = aws_security_group.elk_sg.id
-# }
-
-# resource "aws_security_group_rule" "elk_alb" {
-#   type                     = "ingress"
-#   from_port                = 80
-#   to_port                  = 80
-#   protocol                 = "tcp"
-#   source_security_group_id = aws_security_group.elk_alb_sg.id
-#   security_group_id        = aws_security_group.elk_sg.id
-# }
